af6b84f5126f25ee3351baa8a231db861780e75f
Simran Gangwani <simrangngwani3107@gmail.com>
2022-01-02 15:36:39 +0530

    intermediate js (sect. 10)

--- 6-JavaScript/app.js
+++ 6-JavaScript/app.js
@@ -120,29 +120,29 @@ x += y;  //x=x+y --> x=8
 
 // we use same naming convention of function name as we did for variable naming
 // 1. vanilla function
-function getMilk() {   
-   console.log("leaveHouse");
-   console.log("moveRight");
-   console.log("moveUp");
-   console.log("moveRight");
-   console.log("moveLeft");
-   console.log("moveDown");
-   console.log("moveLeft");
-   console.log("enterHouse"); 
-}
-getMilk();
-
-// 2. 
-function getMilk (bottles) {
+function getMilk() {
     console.log("leaveHouse");
     console.log("moveRight");
     console.log("moveUp");
     console.log("moveRight");
-    console.log("buy "+bottles+" milk")
     console.log("moveLeft");
     console.log("moveDown");
     console.log("moveLeft");
-    console.log("enterHouse");  
+    console.log("enterHouse");
+}
+getMilk();
+
+// 2. 
+function getMilk(bottles) {
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("buy " + bottles + " milk")
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("enterHouse");
 }
 
 getMilk(2);
@@ -180,14 +180,14 @@ function getMilk(money) {
     console.log("moveRight");
     console.log("moveUp");
     console.log("moveRight");
-    var numberOfBottles = Math.floor(money /1.5);
-    console.log("buy "+ numberOfBottles+" bottles of milk")
+    var numberOfBottles = Math.floor(money / 1.5);
+    console.log("buy " + numberOfBottles + " bottles of milk")
     console.log("moveLeft");
     console.log("moveDown");
     console.log("moveLeft");
     console.log("enterHouse");
 
-    return money%1.5;
+    return money % 1.5;
 }
 
 var change = getMilk(4);
@@ -200,7 +200,7 @@ function getMilk(money, costPerBottle) {
     console.log("moveRight");
     console.log("moveUp");
     console.log("moveRight");
-    console.log("buy "+calcBottles(money, costPerBottle)+ " bottles of milk")
+    console.log("buy " + calcBottles(money, costPerBottle) + " bottles of milk")
     console.log("moveLeft");
     console.log("moveDown");
     console.log("moveLeft");
@@ -210,7 +210,7 @@ function getMilk(money, costPerBottle) {
 }
 
 function calcBottles(startingMoney, costPerBottle) {
-    var numberOfBottles= Math.floor(startingMoney/costPerBottle);
+    var numberOfBottles = Math.floor(startingMoney / costPerBottle);
     return numberOfBottles;
 }
 
@@ -219,13 +219,162 @@ function calcChange(startingAmount, costPerBottle) {
     return change;
 }
 
-console.log("Hello master, here is your "+getMilk(5, 1.5)+ " change");
+console.log("Hello master, here is your " + getMilk(5, 1.5) + " change");
 
 // TASK: calculate the BMI
 function bmiCalculator(height, weight) {
-    var bmi= weight/ Math.pow(height,2);
+    var bmi = weight / Math.pow(height, 2);
     return Math.round(bmi);
 }
 
-var bmi= bmiCalculator(60, 1.8);
+var bmi = bmiCalculator(60, 1.8);
 console.log(bmi);
+
+// *******************************
+// Section 10: Intermmediate JavaScript
+// *******************************
+
+// Math.random()   --> this generates a 16 digit decimal number b/w 0 to 0.9999999999999999 --> it never gives 1
+
+var n = Math.random(); //psuedo random generator as comp. is a giant box of on-off (1-0) switches and comp. do all its work through these switches
+
+// refer KHAN ACADMEY VIDEO to understand diff b/w RANDOM & PSEUDO RANDOM
+
+console.log(n);  // 0.43456787623986533
+n = n * 6;  // 2.607407257439192  --> by this the highest no. we'll get is 5.99 it never reaches 6
+//*BY 6 AS WE WANTED NUMBERS TILL 6
+n = Math.floor(n) + 1;   //by Math.floor it will always show rage 0-5 so adding 1
+console.log(n)      //1,2,3,4,5,6
+
+// TASK: MAKE A LOVE CALCULATOR
+var firstName= prompt('Enter first name');
+var secondName= prompt('Enter second name');
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+
+if (loveScore > 70) {
+    alert('Your love score is '+loveScore+" %. You love each other like Kanye love Kanye")
+} if (loveScore > 30 && loveScore <= 70) {
+    alert('Your love score is '+loveScore+" %.")
+}
+if (loveScore <= 30) {
+    alert("Your love score is "+loveScore+"%. You go together like oil and water")
+}
+
+// COMPARATOR OPERATORS
+// === --> is equal to (CHECKS DATA TYPE AS WELL)
+// !== --> is not equal to
+// == --> (ONLY CHECKS VALUE, DOES NOT CHECK DATATYPE)
+
+var a = 1;
+var b = "1";
+typeof (a);  //number
+typeof (b);  //string
+// a===b ---> FALSE
+// a==b ---> TRUE
+
+// && AND (if cond1 is true AND cond2 is true)
+// || OR (if EITHER cond1 is true OR cond2 is true)
+// ! NOT
+
+
+//TASK: LEAP YEAR CALCULATOR
+function isLeap(year) {
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return "leap year"
+            } else {
+                return 'not leap year'
+            }
+        } else {
+            return "leap year"
+        }
+    } else {
+        return "not leap year"
+    }
+}
+// refer FLOWCHART from IMAGES folder
+
+// ARRAY: collection of related data in same container 
+
+var guestList=['simran', 'manav', 'naisha', 'yash'];
+var guestName= prompt('What is your name?')
+ if(guestList.includes(guestName)){
+ alert('You are welcome '+guestName)
+ }else{
+ alert("Sorry, "+guestName+". May be next time.")
+ }
+
+// TASK: Write a prog. that prints numbers from 1 to 100. But for multiples of 3 prints "Fizz" instead of the number and for multiples of five print "Buzz". For number which are multiples of both three and five print "FizzBuzz"
+var output = [];
+var count = 1;
+
+function fizzBuzz() {
+    for (var count = 1; count < 101; count++) {
+        if (count % 3 === 0 && count % 5 === 0) {
+            output.push("Fizz-Buzz")
+        }
+        else if (count % 3 === 0) {
+            output.push("Fizz")
+        } else if (count % 5 === 0) {
+            output.push("Buzz")
+        } else {
+            output.push(count)
+        }
+    }
+    console.log(output)
+}
+
+fizzBuzz()
+
+
+// TASK: WHOS'S BUYING LUNCH
+var names = ['simran', 'manav', 'yash', 'rohan', 'naisha']
+function whosPaying(names) {
+    var numberOfPeople = names.length;
+    var randomPersonPosition = Math.floor(Math.random() * numberOfPeople);
+    var randomPerson = names[randomPersonPosition];
+
+    return randomPerson + " is going to buy lunch today!";
+}
+
+var payingName = whosPaying(names);
+console.log(payingName)
+
+// TASK: TO PRINT THE SONG 99 BOTTLES OF BEER SONG USING WHILE LOOP
+var numberOfBottles = 99
+while (numberOfBottles >= 0) {
+    var bottleWord = "bottle";
+    if (numberOfBottles === 1) {
+        bottleWord = "bottles";
+    }
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+    console.log(numberOfBottles + " " + bottleWord + " of beer,");
+    console.log("Take one down, pass it around,");
+    numberOfBottles--;
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
+}
+
+// TASK: FIBONACCI SERIES
+// REFER images folder for FLOWCHART
+function fibonacciGenerator(n) {
+    var output = [];
+    if (n === 1) {
+        output = [0];
+    }
+    else if (n === 2) {
+        output = [0, 1]
+    }
+    else {
+        output = [0, 1];
+        for (var i = 2; i < n; i++) {
+            output.push(output[output.length - 2] + output[output.length - 1]);
+        }
+    }
+    return output;
+}
+
+output = fibonacciGenerator(5);
+console.log(output);    //[0, 1, 1, 2, 3]
